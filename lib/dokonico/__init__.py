826e2785d856b1b710df6722388e115287c9ec7a
kanosaki <kanosaki@gmail.com>
2012-03-12 13:47:42 +0900

    Edit applicaiton beginning logic

--- lib/dokonico/__init__.py
+++ lib/dokonico/__init__.py
@@ -1,5 +1,21 @@
 
 
+DEFAULT_CONF_PATH = "etc/config.json"
+
 def start(options = {}):
+    conf_path = options.get("conf_path") or DEFAULT_CONF_PATH
+    start_sync(conf_path)
+
+def _init_app(conf_path):
+    from dokonico import loader
+    ldr = loader.AppLoader(conf_path)
+    return ldr.load()
+    
+
+def start_sync(conf_path):
+    app = _init_app()
+    app.sync_latest()
+
+def show_sessions(conf_path):
     pass
     
