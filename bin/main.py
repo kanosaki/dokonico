80c95e9e618f78ee13106078e032176f455a76cf
kanosaki <kanosaki@gmail.com>
2012-03-20 14:39:24 +0900

    Add Python version checking

--- bin/main.py
+++ bin/main.py
@@ -2,6 +2,10 @@
 import sys
 import os
 
+if sys.version < '3':
+    print("This application supports Python 3 or later.")
+    sys.exit(-1)
+
 APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 LIB_DIR = os.path.join(APP_ROOT, "lib")
 sys.path.insert(0, LIB_DIR)
